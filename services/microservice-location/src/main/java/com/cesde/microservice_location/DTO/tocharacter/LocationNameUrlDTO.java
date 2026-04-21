ff988a5caf04a8f5c8d88a3ea09eb8b491b5e796
Brayan Quebrada <alexisquebrada11@gmail.com>
2026-04-21 16:09:38 -0500

    contrato con characters funcionando

---
+++ services/microservice-location/src/main/java/com/cesde/microservice_location/DTO/tocharacter/LocationNameUrlDTO.java
@@ -0,0 +1,16 @@
+package com.cesde.microservice_location.DTO.tocharacter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationNameUrlDTO {
+
+	String name;
+	String url;
+}
--- services/microservice-location/src/main/java/com/cesde/microservice_location/repository/LocationRepository.java
+++ services/microservice-location/src/main/java/com/cesde/microservice_location/repository/LocationRepository.java
@@ -1,14 +1,18 @@
 package com.cesde.microservice_location.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cesde.microservice_location.DTO.tocharacter.LocationNameUrl;
 import com.cesde.microservice_location.entity.LocationRickAndMorty;
 
 @Repository
 public interface LocationRepository extends JpaRepository<LocationRickAndMorty, Integer>{
 
 	Page<LocationRickAndMorty> findAll(Pageable pageable);
+	Optional<LocationNameUrl> findNameAndUrlById(Integer id);
 }
